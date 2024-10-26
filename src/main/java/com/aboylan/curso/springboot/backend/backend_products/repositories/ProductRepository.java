26e00ead8b8fc13f4be077359d68af18fc081784
Alex Boylan <aboylan@users.noreply.github.com>
2024-10-25 21:21:50 -0600

    Configurando Spring Data Rest Crud

--- src/main/java/com/aboylan/curso/springboot/backend/backend_products/repositories/ProductRepository.java
+++ src/main/java/com/aboylan/curso/springboot/backend/backend_products/repositories/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.aboylan.curso.springboot.backend.backend_products.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.aboylan.curso.springboot.backend.backend_products.entities.Product;
 
+@RepositoryRestResource(path = "products")
 public interface ProductRepository extends CrudRepository<Product, Long> {
 
 }
